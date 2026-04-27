0090f6998f417152ca11a0c4ec427249107eefa4
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2026-04-27 08:43:29 +0200

    Uncomment some lines

--- metbrief.py
+++ metbrief.py
@@ -123,21 +123,21 @@ def main():
         #                        f'{var_model}/{model_info["today_model"]}-{hour_model}00z.html')
         #                 download_kachelmann(s, url, user_agent, type_data='model', loc_in=None, model_var=var_model)
 
-        # # Download kachelmannwetter.com soundings images
-        # if 'sounding_dict' in keys_charts:
-        #     today_sounding = datetime.date.today().strftime('%Y%m%d')
-        #     for station, area_id_sounding in detail_comp[LOC_COMP]['sounding_dict'].items():
-        #         url = (f'https://kachelmannwetter.com/de/ajax/obsdetail?station_id=R{station}'
-        #                f'&timestamp={today_sounding}0000&param_id=1&model=obsradio'
-        #                f'&area_id={area_id_sounding}&counter=true&lang=DE')
-        #         download_kachelmann(s, url, 'sounding')
-        #
-        # # Download DWD charts
-        # if not os.path.isdir('gwl'):
-        #     os.mkdir('gwl')
-        # for chart in ['bwk_bodendruck_na_ana', 'ico_500ht_na_ana']:
-        #     file_url = f'https://www.dwd.de/DWD/wetter/wv_spez/hobbymet/wetterkarten/{chart}.png'
-        #     request_download(file_url, user_agent, opath='gwl/')
+        # Download kachelmannwetter.com soundings images
+        if 'sounding_dict' in keys_charts:
+            today_sounding = datetime.date.today().strftime('%Y%m%d')
+            for station, area_id_sounding in detail_comp[LOC_COMP]['sounding_dict'].items():
+                url = (f'https://kachelmannwetter.com/de/ajax/obsdetail?station_id=R{station}'
+                       f'&timestamp={today_sounding}0000&param_id=1&model=obsradio'
+                       f'&area_id={area_id_sounding}&counter=true&lang=DE')
+                download_kachelmann(s, url, 'sounding')
+
+        # Download DWD charts
+        if not os.path.isdir('gwl'):
+            os.mkdir('gwl')
+        for chart in ['bwk_bodendruck_na_ana', 'ico_500ht_na_ana']:
+            file_url = f'https://www.dwd.de/DWD/wetter/wv_spez/hobbymet/wetterkarten/{chart}.png'
+            request_download(file_url, user_agent, opath='gwl/')
 
         # Set variables that should be downloaded from topmeteo
         var_topmeteo = {'pfd': 28, 'thermik': 24, 'wolken': 26, 'wind_1500': 39}
@@ -149,8 +149,8 @@ def main():
         download_topmeteo(var_topmeteo, loc=detail_comp[LOC_COMP]['loc_topmeteo'],
                           day=0, today=today, user=USERNAME_TOPMETEO, passwd=PASSWORD_TOPMETEO)
 
-        # # Download wetter3
-        # request_download('https://wetter3.de/Animation_00_UTC/12_10.gif', user_agent, opath='gwl/')
+        # Download wetter3
+        request_download('https://wetter3.de/Animation_00_UTC/12_10.gif', user_agent, opath='gwl/')
 
     if create_presentation_locally:
         # Verify if command-line LibreOffice is available
